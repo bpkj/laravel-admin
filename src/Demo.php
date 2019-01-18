e220e0b78f30bd73bbc99a8c9075128846f30977
佰鹏科技 <548537950@qq.com>
2019-01-19 02:00:20 +0800

    Add files via upload

---
+++ src/Demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZDZX
+ * Date: 2019/1/19
+ * Time: 1:48
+ */
+
+namespace zdzx;
+
+class Demo
+{
+
+    public static function hello(string $str){
+       echo  $str;
+    }
+
+}
---
+++ src/app/Demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZDZX
+ * Date: 2019/1/19
+ * Time: 1:52
+ */
+
+namespace zdzx\app;
+
+
+class Demo
+{
+    public static function app($ss){
+        print_r($ss);
+    }
+
+}
